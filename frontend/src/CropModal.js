6a25b9e5eacecef1366c53bbb8cb077e804db9d8
Bolden Jones <jonesbolden85@gmail.com>
2024-10-06 12:32:15 -0500

    modals

---
+++ frontend/src/CropModal.js
@@ -0,0 +1,28 @@
+// src/components/CropModal.js
+import React from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root'); // Set the root element for accessibility
+
+const CropModal = ({ isOpen, onRequestClose, crop }) => {
+    if (!crop) {
+        return null;
+    }
+    return (
+        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+
+            <h2>{crop.name}</h2>
+            <img src={crop.image} alt={crop.title} style={{ width: '100%', height: 'auto' }} />
+            <p>{crop.description}</p>
+            <h3>Pest Management Tips:</h3>
+            <p>{crop.tips.pestManagement}</p>
+            <h3>Pesticide Recommendations:</h3>
+            <p>{crop.tips.pesticide}</p>
+            <h3>Water Requirements:</h3>
+            <p>{crop.tips.water}</p>
+            <button onClick={onRequestClose}>Close</button>
+        </Modal>
+    );
+};
+
+export default CropModal;
